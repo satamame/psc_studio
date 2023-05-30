f0170544e0b9f83fb86cf08e235404e19d2ad5d7
satamame <satax@nifty.com>
2023-05-31 00:06:24 +0900

    front initial and sample projects

---
+++ psst_front/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+  <title>PSSt Home</title>
+  <meta name="description" content="PSc Studio Home" />
+</svelte:head>
+
+<h1>PSc Studio Home</h1>
---
+++ psst_front/src/routes/script/+page.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+  <title>PSSt Script</title>
+  <meta name="description" content="PSc Studio Script" />
+</svelte:head>
+
+<h1>PSc Studio Script</h1>
