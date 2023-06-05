224d65bdad7f49f44e2ad5d4f0373959458bbad6
satamame <satax@nifty.com>
2023-06-05 23:59:36 +0900

    serializer の親クラスを変更

--- psst_back/psc/serializers.py
+++ psst_back/psc/serializers.py
@@ -3,20 +3,19 @@ from rest_framework import serializers
 from .models import PlayScript, Character, Scene
 
 
-class PlayScriptSerializer(serializers.HyperlinkedModelSerializer):
+class PlayScriptSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlayScript
         fields = '__all__'
-        # fields = ['url', 'owner', 'title', 'plot']
 
 
-class CharacterSerializer(serializers.HyperlinkedModelSerializer):
+class CharacterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Character
         fields = '__all__'
 
 
-class SceneSerializer(serializers.HyperlinkedModelSerializer):
+class SceneSerializer(serializers.ModelSerializer):
     class Meta:
         model = Scene
         fields = '__all__'
